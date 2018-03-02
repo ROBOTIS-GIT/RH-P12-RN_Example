47407c7ffd16e756065aecae853dc59a5e36fffb
ROBOTIS-zerom <zerom@robotis.com>
2018-03-02 20:23:49 +0900

    LICENSE changed. (BSD -> Apache 2.0)

--- rh-p12-rn.cpp
+++ rh-p12-rn.cpp
@@ -1,5 +1,18 @@
-// RH-P12-RN-Example.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
-//
+/*******************************************************************************
+* Copyright 2017 ROBOTIS CO., LTD.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
 
 #if defined(__linux__)
 #include <unistd.h>
